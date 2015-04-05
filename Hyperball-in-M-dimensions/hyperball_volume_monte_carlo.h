808d99704de692d9f9a16e6b392aaf666b2853c6
Ben Bubnick <ben.bubnick@gmail.com>
2015-04-05 18:11:48 -0400

    Create hyperball_volume_monte_carlo.h

---
+++ Hyperball-in-M-dimensions/hyperball_volume_monte_carlo.h
@@ -0,0 +1,7 @@
+int main ( int argc, char *argv[] );
+double *hyperball01_indicator ( int dim_num, int point_num, double x[] );
+double hyperball01_volume ( int dim_num );
+double r8_abs ( double x );
+double *r8mat_uniform_01_new ( int m, int n, int *seed );
+double r8vec_sum ( int n, double a[] );
+void timestamp ( );
